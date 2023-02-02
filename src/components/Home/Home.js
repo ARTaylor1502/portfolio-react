e8f6104b5c606d49c2614a90f7d5f90083ed69cd
Alex Taylor <alex.taylor@and.digital>
2023-02-02 22:32:22 +0000

    Add mobile stylings to home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,30 +1,40 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => (
   <div>
-    <h2 className='mb-8 text-2xl font-semibold'>Hi I'm Alex.</h2>
-    <div className='grid grid-cols-3'>
-      <div className='pr-20 text-justify'>
+    <h2 className="mb-8 py-4 text-2xl font-semibold">Hi I'm Alex.</h2>
+    <div className="grid rid-cols-1 sm:grid-cols-3">
+      <div className="mb-6 lg:pr-20 text-justify">
         <p>
-          I am a full stack web developer with 6 years commercial experience and a 2:1 BSc (hons) in Web Development from Stafford University.
-          I initially began website development as a hobby around 2012 and well and truly caught the coding bug from that point on.
+          I am a full stack web developer with 6 years commercial experience and
+          a 2:1 BSc (hons) in Web Development from Stafford University. I
+          initially began website development as a hobby around 2012 and well
+          and truly caught the coding bug from that point on.
         </p>
       </div>
-      <div className='pr-20 text-justify'>
+      <div className="mb-6 lg:pr-20 text-justify">
         <p>
-          During my time working in the industry I have built bespoke software such as CRMs and Project Managements solutions.
-          I've also built everything from large scale e-commerce platforms to mobile applications and CMS websites.
+          During my time working in the industry I have built bespoke software
+          such as CRMs and Project Managements solutions. I've also built
+          everything from large scale e-commerce platforms to mobile
+          applications and CMS websites.
         </p>
       </div>
-      <div className='text-justify'>
+      <div className="text-justify">
         <p>
-          During my time working in the industry I have built bespoke software such as CRMs and Project Managements solutions.
-          I've also built everything from large scale e-commerce platforms to mobile applications and CMS websites.
+          During my time working in the industry I have built bespoke software
+          such as CRMs and Project Managements solutions. I've also built
+          everything from large scale e-commerce platforms to mobile
+          applications and CMS websites.
         </p>
       </div>
     </div>
     <div>
-      <img className='my-24 mx-auto w-6/12' src='desk.svg' alt='Desk' />
+      <img
+        className="my-12 lg:my-24 mx-auto w-100 lg:w-6/12"
+        src="desk.svg"
+        alt="Desk"
+      />
     </div>
   </div>
 );
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,32 +1,45 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => (
-  <div className='flex flex-col h-screen leading-8'>
-    <header className='py-12'>
-      <div className='mx-auto container flex'>
-        <a href='/'>
-          <img className='w-80' src='logo.svg' alt='ARTaylor Web Development'/>
+  <div className="flex flex-col h-screen leading-8">
+    <header className="p-6 lg:py-12">
+      <div className="mx-auto container flex">
+        <a href="/">
+          <img
+            className="w-100 h-28 lg:w-80"
+            src="logo.svg"
+            alt="ARTaylor Web Development"
+          />
         </a>
       </div>
     </header>
-    <main className='mx-auto mb-auto py-12 container grow'>
+    <main className="mx-auto mb-auto px-6 lg:py-12 lg:px-6 container grow">
       <Outlet />
     </main>
-    <footer className='py-6'>
-      <div className='mx-auto container'>
-        <div className='flex justify-center'>
-          <a href='http://uk.linkedin.com/in/artaylorwebdesign' target='_blank'>
-            <i className='m-6 text-4xl fa fa-linkedin' aria-hidden='true'></i>
-            </a>
-          <a href='mailto:artaylor1502@gmail.com?Subject=ARTaylorWebDevelopment%20Query' target='_blank'>
-            <i className='m-6 text-4xl fa fa-envelope' aria-hidden='true'></i>
+    <footer className="p-6 pt-0 lg:py-6">
+      <div className="mx-auto container">
+        <div className="flex justify-center">
+          <a href="http://uk.linkedin.com/in/artaylorwebdesign" target="_blank">
+            <i className="m-6 text-4xl fa fa-linkedin" aria-hidden="true"></i>
           </a>
-          <a href='https://github.com/ARTaylor1502' target='_blank'>
-            <i className='m-6 text-4xl fa fa-github' aria-hidden='true'></i>
+          <a
+            href="mailto:artaylor1502@gmail.com?Subject=ARTaylorWebDevelopment%20Query"
+            target="_blank"
+          >
+            <i className="m-6 text-4xl fa fa-envelope" aria-hidden="true"></i>
+          </a>
+          <a href="https://github.com/ARTaylor1502" target="_blank">
+            <i className="m-6 text-4xl fa fa-github" aria-hidden="true"></i>
           </a>
         </div>
-        <span>ARTaylor Web Development ©<script type="text/javascript">document.write(new Date().getFullYear());</script>2023</span>
+        <p className="mx-auto text-center">
+          ARTaylor Web Development ©
+          <script type="text/javascript">
+            document.write(new Date().getFullYear());
+          </script>
+          2023
+        </p>
       </div>
     </footer>
   </div>
