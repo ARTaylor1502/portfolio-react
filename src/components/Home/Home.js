8188e59c801b6c35fe185e079d60c3ad40a215a2
Alex Taylor <alex.taylor@and.digital>
2023-02-05 20:31:56 +0000

    Update Home content and index

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,8 +6,8 @@ const Home = () => (
     <div className="grid rid-cols-1 md:grid-cols-3">
       <div className="mb-6 md:mb-0 md:pr-6 text-justify">
         <p className="text-lg leading-loose">
-          I am a full stack web developer with 6 years commercial experience and
-          a 2:1 BSc (hons) in Web Development from Stafford University. I
+          I'm Alex a full stack web developer with 7 years commercial experience
+          and a 2:1 BSc (hons) in Web Development from Stafford University. I
           initially began website development as a hobby around 2012 and well
           and truly caught the coding bug from that point on.
         </p>
@@ -22,10 +22,11 @@ const Home = () => (
       </div>
       <div className="md:pl-6 text-justify">
         <p className="text-lg leading-loose">
-          During my time working in the industry I have built bespoke software
-          such as CRMs and Project Managements solutions. I've also built
-          everything from large scale e-commerce platforms to mobile
-          applications and CMS websites.
+          I predominantly work as a full stack developer but have recently
+          focused on front end work. My last commercial project saw me working
+          on a React pensions projection app for Scottish Widows. In my spare
+          time I've also worked on various projects using React, Ember js and
+          Vue js.
         </p>
       </div>
     </div>
